f0ed8526b665052e8539713266457829f79c2810
odrugees <odrugees@poligran.edu.co>
2021-04-18 14:34:22 -0500

    Primera Carga creacion Proyecto y Repositorio GIT

---
+++ src/test/java/com/edu/poli/proyecto/ProyectoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.poli.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
